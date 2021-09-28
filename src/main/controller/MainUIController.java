03dc928e8868575cbee00fcaefcfbe3cfdaa7bd6
Nguyen Tuan Dat <s3726128@student.rmit.edu.au>
2021-09-28 10:35:57 +0700

    Adding timer countdown on top panel

---
+++ src/main/controller/MainUIController.java
@@ -0,0 +1,211 @@
+package main.controller;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+import main.FxmlLoader;
+import main.module.MainUIModule;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainUIController implements Initializable
+{
+    public MainUIModule mainUIModule = new MainUIModule();
+    @FXML
+    private AnchorPane slider;
+    @FXML
+    private ImageView Exit;
+    @FXML
+    private Label nightTime;
+    @FXML
+    private Label dailyTime;
+    @FXML
+    private Label impTime;
+    @FXML
+    private Label Menu;
+    @FXML
+    private Label MenuClose;
+    @FXML
+    private BorderPane mainPane;
+
+    long night_hour,night_minute,night_second,daily_hour,daily_minute,daily_second,imp_hour,imp_minute,imp_second;
+
+    Timeline timeline = new Timeline(
+            new KeyFrame(Duration.seconds(1),
+                    e -> {
+                        oneSecondPass();
+                        timeDisplay();
+                    })
+    );
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        Exit.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+        slider.setTranslateX(0);
+        Menu.setOnMouseClicked(event -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(slider);
+
+            slide.setToX(0);
+            slide.play();
+
+            slider.setTranslateX(-270);
+
+            slide.setOnFinished((ActionEvent e)-> {
+                Menu.setVisible(false);
+                MenuClose.setVisible(true);
+            });
+        });
+
+        MenuClose.setOnMouseClicked(event -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(slider);
+
+            slide.setToX(-270);
+            slide.play();
+
+            slider.setTranslateX(0);
+
+            slide.setOnFinished((ActionEvent e)-> {
+                Menu.setVisible(true);
+                MenuClose.setVisible(false);
+            });
+        });
+
+        //display default content for the first time
+        FxmlLoader object = new FxmlLoader();
+        Pane view = object.getPage("Home");
+        mainPane.setCenter(view);
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
+        timeCalculator();
+    }
+
+    public void timeCalculator()
+    {
+        long night_temp = mainUIModule.getNightTime();
+        night_hour = night_temp / 3600;
+        night_minute = (night_temp % 3600) / 60;
+        night_second = night_temp - night_hour * 3600 - night_minute * 60;
+
+        daily_hour = mainUIModule.getDailyTime() / 3600;
+        daily_minute = (mainUIModule.getDailyTime() % 3600) / 60;
+        daily_second = mainUIModule.getDailyTime() - daily_hour * 3600 - daily_minute * 60;
+
+        imp_hour = mainUIModule.getImperialTime() / 3600;
+        imp_minute = (mainUIModule.getImperialTime() % 3600) / 60;
+        imp_second = mainUIModule.getImperialTime() - imp_hour * 3600 - imp_minute * 60;
+    }
+
+    public void oneSecondPass()
+    {
+        night_second--;
+        if (night_second < 0)
+        {
+            night_minute--;
+            if (night_minute < 0)
+            {
+                if (night_hour <= 0)
+                    timeCalculator();
+            }
+            else
+                night_second = 59;
+
+            if (night_minute < 0)
+            {
+                night_hour--;
+                night_minute = 59;
+            }
+        }
+
+        daily_second--;
+        if (daily_second < 0)
+        {
+            daily_minute--;
+            if (daily_minute < 0)
+            {
+                if (daily_hour <= 0)
+                    timeCalculator();
+            }
+            else
+                daily_second = 59;
+            if (daily_minute < 0)
+            {
+                daily_hour--;
+                daily_minute = 59;
+            }
+        }
+
+        imp_second--;
+        if (imp_second < 0)
+        {
+            imp_minute--;
+            if (imp_minute < 0)
+            {
+                if (imp_hour <= 0)
+                    timeCalculator();
+            }
+            else
+                imp_second = 59;
+            if (imp_minute < 0)
+            {
+                imp_hour--;
+                imp_minute = 59;
+            }
+        }
+    }
+
+    public void timeDisplay()
+    {
+        if (night_hour <= 0)
+            nightTime.setText(night_minute + "m " + night_second + "s");
+        else
+            nightTime.setText(night_hour + "h " + night_minute + "m " + night_second + "s");
+
+        if (daily_hour <= 0)
+            dailyTime.setText(daily_minute + "m " + daily_second + "s");
+        else
+            dailyTime.setText(daily_hour + "h " + daily_minute + "m " + daily_second + "s");
+
+        if (imp_hour <= 0)
+            impTime.setText(imp_minute + "m " + imp_second + "s");
+        else
+            impTime.setText(imp_hour + "h " + imp_minute + "m " + imp_second + "s");
+    }
+
+    //display Home tab inside homepage when click on booking button
+    @FXML
+    private void loadHome(ActionEvent event) throws IOException
+    {
+        FxmlLoader object = new FxmlLoader();
+        Pane view = object.getPage("Home");
+        mainPane.setCenter(view);
+    }
+
+    //display Date/Time tab inside homepage when click on booking button
+    @FXML
+    private void loadDateTime(ActionEvent event) throws IOException
+    {
+        FxmlLoader object = new FxmlLoader();
+        Pane view = object.getPage("DateTime");
+        mainPane.setCenter(view);
+    }
+}
---
+++ src/main/module/MainUIModule.java
@@ -0,0 +1,65 @@
+package main.module;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+public class MainUIModule
+{
+    String[] nightTime = new String[]{"02:40:00", "06:40:00", "10:40:00", "14:40:00", "18:40:00", "22:40:00"};
+    String[] imperialTime = new String[]{"01:00:00", "04:00:00", "07:00:00", "10:00:00", "13:00:00", "16:00:00", "19:00:00", "22:00:00"};
+
+    public long getNightTime()
+    {
+        int i = 1;
+        boolean check = false;
+        long time = 0;
+        while(!check && i<= nightTime.length)
+        {
+            LocalTime localTime = LocalTime.now();
+            LocalTime timeFixed = LocalTime.parse(nightTime[i]);
+            if (timeFixed.compareTo(localTime) > 0)
+            {
+                time = localTime.until(timeFixed, ChronoUnit.SECONDS);
+                check = true;
+            }
+            else
+                i++;
+        }
+        return time;
+    }
+
+    public long getDailyTime()
+    {
+        LocalTime localTime = LocalTime.now();
+        long time = 0;
+        LocalTime timeFixed = LocalTime.parse("05:00:00");
+        if (timeFixed.compareTo(localTime) > 0)
+            time = localTime.until(timeFixed, ChronoUnit.SECONDS);
+        else
+        {
+            time = 86400 - timeFixed.until(localTime, ChronoUnit.SECONDS);
+        }
+        return time;
+    }
+
+    public long getImperialTime()
+    {
+        LocalTime localTime = LocalTime.now();
+        int i = 1;
+        boolean check = false;
+        long time = 0;
+        while(!check && i<= imperialTime.length)
+        {
+            LocalTime timeFixed = LocalTime.parse(imperialTime[i]);
+            if (timeFixed.compareTo(localTime) > 0)
+            {
+                time = localTime.until(timeFixed, ChronoUnit.SECONDS);
+                check = true;
+            }
+            else
+                i++;
+        }
+        return time;
+    }
+}
