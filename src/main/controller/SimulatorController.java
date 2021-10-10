658074d277f642e4cc9072ce0d9159cc581e6a62
Nguyen Tuan Dat <s3726128@student.rmit.edu.au>
2021-10-10 22:48:34 +0700

    adding data and percent to each equipment fixing UI

--- src/main/controller/SimulatorController.java
+++ src/main/controller/SimulatorController.java
@@ -1,9 +1,7 @@
 package main.controller;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import javafx.animation.FadeTransition;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,11 +9,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.animation.TranslateTransition;
-import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -23,10 +18,8 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.util.Duration;
-import main.FxmlLoader;
 import main.module.SimulatorModule;
 
-import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -36,29 +29,33 @@ public class SimulatorController implements Initializable
 {
     String[] simulator = {"Enhancement", "Fairy Sprouting", "Pet Exchange", "Horse Leveling"};
     String[] enhanceEquip = {"Weapon", "Armor", "Accessory"};
-    String[] enhanceType = {"Blue/Yellow", "Green", "Blackstar", "Fallen God's", "Tuvala"};
+    String[] enhanceType = {"Blue-Yellow", "Green", "Blackstar", "Fallen God's", "Tuvala"};
     String[] enhanceLevel = {"9->10","10->11","12->13","13->14","14->15","15->PRI","PRI->DOU", "DOU->TRI", "TRI->TET", "TET->PEN"};
     SimulatorModule simulatorModule = new SimulatorModule();
-    String functionOption = "";     //set it to default option
+    String functionOption = "";
+    int totalFailstack;
 
-    boolean isSelected;
+    boolean realBoxIsSelected, muteBoxIsSelected;
+    boolean addFailstackIsClicked = false;
 
     @FXML
     private ComboBox<String> function,equip,type,level;
     @FXML
     private Button appearBox, applyButton;
     @FXML
-    private ImageView realIcon, simulatorIcon, enhanceImg;
+    private ImageView realIcon, simulatorIcon;
     @FXML
-    private Pane realBox, simulatorBox, equipPane, typePane, levelPane, enhanceUI;
+    private Pane realBox, simulatorBox, equipPane, typePane, levelPane, enhanceUI, addFailstackBox;
     @FXML
-    private CheckBox realCheckBox;
+    private CheckBox realCheckBox,muteBox;
     @FXML
     private AnchorPane enhancePane;
     @FXML
-    private Label equipStore,levelStore,typeStore;
+    private Label equipStore,levelStore,typeStore, successCount, failCount, failstackDisplay, totalAddedRateDisplay, totalPercentDisplay;
     @FXML
     private MediaView enhanceVid;
+    @FXML
+    private TextField failstackTextField;
 
     //for fade transition
     private FadeTransition fadeIn = new FadeTransition(
@@ -82,20 +79,44 @@ public class SimulatorController implements Initializable
         iconHoverEffect(realIcon, realBox);
         iconHoverEffect(simulatorIcon, simulatorBox);
 
-        //checkbox listener
+        //realCheckBox listener
         realCheckBox.selectedProperty().addListener(
                 (ObservableValue<? extends Boolean> ov, Boolean old_val, Boolean new_val) ->
                 {
-                    isSelected = realCheckBox.isSelected();
+                    realBoxIsSelected = realCheckBox.isSelected();
+                });
+
+        //muteBox listener
+        muteBox.selectedProperty().addListener(
+                (ObservableValue<? extends Boolean> ov, Boolean old_val, Boolean new_val) ->
+                {
+                    muteBoxIsSelected = muteBox.isSelected();
                 });
     }
 
-    public void enhanceEffect()
+    public void enhanceEffect(boolean check)
     {
-        File path = new File("src/main/eff/3.mp4");
+        String filePath;
+        if (check)
+        {
+            filePath = "src/main/eff/short_success.mp4";
+        }
+        else
+        {
+            filePath = "src/main/eff/short_fail.mp4";
+        }
+        File path = new File(filePath);
         MediaPlayer mediaPlayer = new MediaPlayer(new Media(path.toURI().toString()));
         enhanceVid.setMediaPlayer(mediaPlayer);
-        mediaPlayer.setAutoPlay(true);
+        if(muteBoxIsSelected)
+        {
+            mediaPlayer.setVolume(0);
+        }
+        else
+        {
+            mediaPlayer.setVolume(1);
+        }
+        mediaPlayer.play();
     }
 
     public void iconHoverEffect(ImageView icon, Pane box)
@@ -235,7 +256,17 @@ public class SimulatorController implements Initializable
     @FXML
     private void setEnhanceButton(ActionEvent event) throws IOException
     {
-        enhanceEffect();
+
+        if (simulatorModule.getEnhanceResult())
+        {
+            enhanceEffect(true);
+            successCount.setText(String.valueOf(Integer.parseInt(successCount.getText()) + 1));
+        }
+        else
+        {
+            enhanceEffect(false);
+            failCount.setText(String.valueOf(Integer.parseInt(failCount.getText()) + 1));
+        }
     }
 
     @FXML
@@ -243,18 +274,81 @@ public class SimulatorController implements Initializable
     {
         if (levelStore.getText() != "" && levelStore.getText() != null)
         {
-            simulatorModule.getEnhanceSelection(equipStore.getText(),typeStore.getText(),levelStore.getText());
+            simulatorModule.searchCsvLine(0, levelStore.getText(), typeStore.getText(), equipStore.getText());
             enhanceUI.setVisible(true);
             fadeIn.setNode(enhanceUI);
             fadeIn.setFromValue(0.0);
             fadeIn.setToValue(1.0);
             fadeIn.playFromStart();
+            totalPercentDisplay.setText(Math.floor(simulatorModule.calculatePercent(0) * 100) / 100 + "%");
+            totalAddedRateDisplay.setText(Math.floor(simulatorModule.addedRate() * 100) / 100 + "%");
         }
         else
         {
             enhanceUI.setVisible(false);
             System.out.println("Please select lv");
         }
+    }
+
+    @FXML
+    private void appearFailstackBox(ActionEvent event)
+    {
+        // force the field to be numeric only
+        failstackTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    failstackTextField.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        FadeTransition failstackFade = new FadeTransition(Duration.seconds(0.2));
+        if (!addFailstackIsClicked)
+        {
+            addFailstackIsClicked = true;
+            addFailstackBox.setVisible(true);
+            failstackFade.setNode(addFailstackBox);
+            failstackFade.setFromValue(0.0);
+            failstackFade.setToValue(1.0);
+            failstackFade.playFromStart();
+        }
+        else
+        {
+            addFailstackIsClicked = false;
+            failstackFade.setNode(addFailstackBox);
+            failstackFade.setFromValue(1.0);
+            failstackFade.setToValue(0.0);
+            failstackFade.playFromStart();
+        }
+    }
+
+    @FXML
+    private void setAddFailstackButton(ActionEvent event)
+    {
+        if ((failstackTextField.getText().isEmpty()) || (failstackTextField.getText() == ""))
+        {
+            totalFailstack = 0;
+        }
+        else
+        {
+            totalFailstack = Integer.parseInt(failstackTextField.getText());
+        }
+
+        //calculate percentage and display it
+        totalPercentDisplay.setText(Math.floor(simulatorModule.calculatePercent(totalFailstack) * 100) / 100 + "%");
+        totalAddedRateDisplay.setText(Math.floor(simulatorModule.addedRate() * 100) / 100 + "%");
+
+        failstackDisplay.setText("+ " + totalFailstack);
+        //fade transition and set the box fade out
+        FadeTransition failstackFade = new FadeTransition(Duration.seconds(0.2));
+        addFailstackIsClicked = false;
+        failstackFade.setNode(addFailstackBox);
+        failstackFade.setFromValue(1.0);
+        failstackFade.setToValue(0.0);
+        failstackFade.playFromStart();
+        failstackTextField.clear();
     }
 
     public void loadOption()
--- src/main/module/SimulatorModule.java
+++ src/main/module/SimulatorModule.java
@@ -1,9 +1,71 @@
 package main.module;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 public class SimulatorModule
 {
-    public void getEnhanceSelection(String equip, String type, String level)
+    double baseRate, totalRate;
+    int softCap;
+
+    public void searchCsvLine(int searchColumnIndex, String searchString, String type, String equip) throws IOException
     {
-        
+        //read data from file
+        String resultRow = null;
+        BufferedReader br = new BufferedReader(new FileReader("src/main/data/" + type + "_" + equip + ".csv"));
+        String line;
+        while ( (line = br.readLine()) != null )
+        {
+            String[] values = line.split(",");
+            if(values[searchColumnIndex].equals(searchString))
+            {
+                resultRow = line;
+                break;
+            }
+        }
+        br.close();
+        //get the rate
+        String[] split = resultRow.split(",");
+        baseRate = Double.parseDouble(split[1]);
+        softCap = Integer.parseInt(split[2]);
+    }
+
+    public double calculatePercent(int fs)
+    {
+        if (fs < softCap)
+        {
+            totalRate = baseRate * (1 + 0.1 * fs);
+        }
+        else
+        {
+            totalRate = baseRate * (1 + 0.1 * softCap) + (baseRate * (1 + (0.02 * (fs- softCap))));
+        }
+
+        if (totalRate >= 90)
+            totalRate = 90;
+
+        return totalRate;
+    }
+
+    public double addedRate()
+    {
+        return (totalRate - baseRate);
+    }
+
+    public boolean getEnhanceResult()
+    {
+        {
+            int max = 100;
+            int min = 0;
+            Random random = new Random();
+            double randomResult = min + (max - min) * random.nextDouble();
+
+            if (randomResult <= totalRate)
+                return true;
+            else
+                return false;
+        }
     }
 }
